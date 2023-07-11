18f318a83d742446fb97071ff0ce1e42270e12fe
probablyjassin <aouanijassin@gmail.com>
2023-07-11 18:14:16 +0200

    mroe fixes

--- cogs/help.py
+++ cogs/help.py
@@ -8,43 +8,43 @@ class help(commands.Cog):
     @commands.command(name = "help.ping")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            value = 'Bro "ping" "pong" was ist daran so schwer'
+            description = 'Bro "ping" "pong" was ist daran so schwer'
         ))
 
     @commands.command(name = "help.say")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            name="say \{user to imitate (optional), message\}", 
-            value="Sends a message of the provided text, as the user to imitate (if given).", inline=False
+            title="say \{user to imitate (optional), message\}", 
+            description="Sends a message of the provided text, as the user to imitate (if given).", inline=False
         ))
 
     @commands.command(name = "help.r")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            name="r \{subreddit\}", 
-            value="Get a reddit post from a subreddit of your choice", inline=False
+            title="r \{subreddit\}", 
+            description="Get a reddit post from a subreddit of your choice", inline=False
         ))
 
     @commands.command(name = "help.quote")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            name="quote \{category\} (optional)", 
-            value="Get a quote, optionally with a category. " +
+            title="quote \{category\} (optional)", 
+            description="Get a quote, optionally with a category. " +
             "Categories include undertale, as well as these: https://api-ninjas.com/api/quotes", inline=False
         ))
 
     @commands.command(name = "help.emote")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            name="emote \{name\} (optional)", 
-            value="Search for 7tv emotes or get a random one!", inline=False
+            title="emote \{name\} (optional)", 
+            description="Search for 7tv emotes or get a random one!", inline=False
         ))
 
     @commands.command(name = "help.henti")
     async def helpi(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(
-            name="henti \{rating, category\} (optional)", 
-            value="Get an some anime imagery. If unspecified, get an sfw catgirl. Optionally use nsfw and/or request a category. " +
+            title="henti \{rating, category\} (optional)", 
+            description="Get an some anime imagery. If unspecified, get an sfw catgirl. Optionally use nsfw and/or request a category. " +
             "Will use default if category is invalid. List of categories: https://pypi.org/project/WaifuPicsPython/", inline=False
         ))  
 
@@ -59,13 +59,12 @@ class help(commands.Cog):
         embed.add_field(
             name = "Commands:", 
             value = f'''
-                ⠀
-                ping
-                say
-                r (reddit)
-                quote
-                emote
-                henti
+            ping
+            say
+            r (reddit)
+            quote
+            emote
+            henti
             '''
         )
         await ctx.send(embed = embed)

4ef3ef84023f6e855baa2d102cca69b990a18954
probablyjassin <aouanijassin@gmail.com>
2024-06-21 19:27:17 +0200

    cool things

--- cogs/experimenting.py
+++ cogs/experimenting.py
@@ -3,6 +3,9 @@ from discord import ApplicationContext, slash_command, Option
 from discord.ui import Button, View
 from discord.ext import commands
 
+import json
+from fuzzywuzzy import fuzz
+
 from waifu import WaifuAioClient
 
 class test(commands.Cog):
@@ -39,6 +42,29 @@ class test(commands.Cog):
         delete_button.callback = button_callback
 
         msg = await ((await ctx.respond(embed=embed, view=view)).original_response())
+
+
+    #data = [item for item in track_data if item["shorthand"] == "TH"][0]
+
+    async def get_tracks(ctx: discord.AutocompleteContext):
+        track_data = []
+        strategy_data = []
+
+        with open("files/tracks.json") as file:
+            track_data = json.loads(file.read())
+            file.close()
+
+        with open("files/strategy.json") as file:
+            strategy_data = json.loads(file.read())
+            file.close()
+
+        scores = [fuzz.ratio(track['name'], ctx.options['track']) for track in track_data]
+        ranked_data = sorted(zip(scores, track_data), key=lambda x: x[0], reverse=True)
+        return [item[1]['code'] for item in ranked_data[:25]]
+
+    @slash_command(name="track_info")
+    async def track_info(self, ctx: ApplicationContext, track = Option(str, autocomplete=discord.utils.basic_autocomplete(get_tracks))):
+        await ctx.respond(track)
 
 def setup(bot: commands.Bot):
     bot.add_cog(test(bot))
